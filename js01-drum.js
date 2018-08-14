8d3d6e9306f3f45b907c2a4174ad61ac0ad5e537
Adrienne Hein <adrienne.hein@gmail.com>
2018-08-14 12:34:21 -0700

    completed

--- js01-drum.js
+++ js01-drum.js
@@ -1,7 +1,21 @@
-window.addEventListener('keydown', function (e) {
+function playSound(e) {
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
   const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
   if(!audio) return;
   audio.currentTime = 0;
   audio.play();
-});
+  key.classList.add('playing');
+};
+
+//remove class that creates transformation
+function removeTransition(e) {
+  if(e.propertyName !== 'transform') return;
+  this.classList.remove('playing');
+}
+
+//transform each key on play
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+
+//play sound on keydown
+window.addEventListener('keydown', playSound);
